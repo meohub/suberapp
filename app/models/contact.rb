814242203c16cc0418b9743e2a8b45bf8d794ee1
Mehmet Ozgul <lemehmet@gmail.com>
2017-03-30 15:23:15 -0400

    Fix: Moved to validates to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 80}
+  validates :surname, presence: true, length: {maximum: 80}
+  validates :phone_number, presence: true, length: {maximum: 16}, numericality:  { only_integer: true }
 end
